2d45a836ad7ebbd61ab81831576e66db29f983c0
tobiasz-p <Tobiasz@DESKTOP-CK0MGLO>
2021-02-20 21:30:10 +0100

    Fix bugs

--- electre3-profiles/electre-tri.py
+++ electre3-profiles/electre-tri.py
@@ -1,48 +1,55 @@
 class Criterium:
-    def __init__(self, types, weight, q, p, v, values_a, values_b):
-        self.type = type
+    c = []
+    d = []
+
+    def __init__(self, types, weight, values_q, values_p, values_v, values_a, values_b):
+        self.types = types
         self.weight = weight
-        self.q = q
-        self.p = p
-        self.v = v
-        self.a = a
-        self.b = b
+        self.values_q = values_q
+        self.values_p = values_p
+        self.values_v = values_v
+        self.values_a = values_a
+        self.values_b = values_b
 
     def calculate_c(self):
-        c = []
-        d = []
-        for type,a,b in self.types, self.values_a, self.values_b:
-            if self.type is 'z':
-                if self.a >= self.b - self.q:
-                    c.append(1)
-                elif self.a > self.b - self.p:
-                    c.append(self.a - (self.b - self.p) / (self.p - self.q))
+        for (type, a, b, q, p, v) in zip(self.types, self.values_a, self.values_b, self.values_q, self.values_p, self.values_v):
+            if type is 'z':
+                if a >= b - q:
+                    self.c.append(1)
+                elif a > b - p:
+                    self.c.append((a - (b - p)) / (p - q))
                 else:
-                    c.append(0)
+                    self.c.append(0)
 
-                if self.a <= self.b - self.v:
-                    d.append(1)
-                elif self.a < self.b - self.p:
-                    d.append(self.b - self.p) - self.a / (self.p - self.v)
+                if a <= b - v:
+                    self.d.append(1)
+                elif a < b - p:
+                    self.d.append(((b - p) - a) / (p - v))
                 else:
-                    d.append(0)
+                    self.d.append(0)
 
-            elif self.type is 'k':
-                if self.a > self.b + self.p:
-                    c.append(0)
-                elif self.a > self.b + self.q:
-                    c.append((self.b + self.p) - self.a / (self.p - self.q))
+            elif type is 'k':
+                if a > b + p:
+                    self.c.append(0)
+                elif a > b + q:
+                    self.c.append(((b + p) - a) / (p - q))
                 else:
-                    c.append(1)
+                    self.c.append(1)
 
-                if self.a > self.b + self.v:
-                    d.append(1)
-                elif self.a > self.b + self.p:
-                    d.append(self.a - (self.b + self.p) / (self.v - self.p))
+                if a > b + v:
+                    self.d.append(1)
+                elif a > b + p:
+                    self.d.append((a - (b + p)) / (v - p))
                 else:
-                    d.append(0)
+                    self.d.append(0)
 
             else:
                 return "type must be 'z' or 'k'"
 
 
+if __name__ == '__main__':
+    # types, weight, values_q, values_p, values_v, values_a, values_b
+    criterium = Criterium(['z', 'k'], [5, 2], [0.3, 0.4], [0.9, 0.8], [1.8, 2.1], [1.7, 7.6], [1.0, 7.0])
+    criterium.calculate_c()
+    print(criterium.c)
+    print(criterium.d)
